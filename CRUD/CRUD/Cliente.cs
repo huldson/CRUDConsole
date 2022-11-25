772a637c7a6dff4a782fcc11a12588daa7a281f6
huldson <huldson-_-@hotmail.com>
2022-11-24 23:28:50 -0300

    adicionar crud

---
+++ CRUD/CRUD/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD
+{
+    internal class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int imoveis { get; set; }
+        public Cliente() { }
+        public Cliente(int id, string nome, int imoveis)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.imoveis = imoveis;
+
+        }
+
+        public override string ToString()
+        {
+            return "id: " + id + "\nnome: " + nome + "\nimoveis: " + imoveis;
+        }
+    }
+}
---
+++ CRUD/CRUD/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { bool sair = true;
+            while (sair) {
+                string caminho = "Server=DESKTOP-BI33SL6;Database=CADASTRO;Trusted_Connection=True;";
+                SqlConnection conexao = new SqlConnection(caminho);
+                Console.Write("qual operação você deseja fazer:\n <salvar>\n <alterar>\n <deletar>\n <exibir>\n:");
+                string operacao = Console.ReadLine();
+                if (operacao == "salvar")
+                {
+                    try
+                    {
+                        string stringDeRetornoDoTamanho = "select count(*) FROM dbo.cliente";
+                        SqlCommand comandoRetorno = new SqlCommand(stringDeRetornoDoTamanho, conexao);
+                        comandoRetorno.CommandType = CommandType.Text;
+                        conexao.Open();
+                        SqlDataReader retornoDoTamanho = comandoRetorno.ExecuteReader();
+                        int tamanho = 0;
+                        if (retornoDoTamanho.Read())
+                        {
+                            tamanho = Convert.ToInt32(retornoDoTamanho[0]);
+                        }
+                        conexao.Close();
+                        Console.Write("nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("imoveis: ");
+                        int imoveis = int.Parse(Console.ReadLine());
+                        conexao.Open();
+                        string stringDeSalvar = $"INSERT INTO dbo.cliente (id, nome, imoveis) VALUES (" + tamanho + ",'" + nome + "' ," + imoveis + " )";
+                        SqlCommand comandoDeSalvar = new SqlCommand(stringDeSalvar, conexao);
+                        comandoDeSalvar.CommandType = CommandType.Text;
+                        int valor = comandoDeSalvar.ExecuteNonQuery();
+                        conexao.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+
+                }
+                else if (operacao == "alterar")
+                {
+                    try
+                    {
+                        Console.Write("id da conta que deseja alterar nome : ");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Write("nome: ");
+                        string nome = Console.ReadLine();
+
+                        conexao.Open();
+                        string stringUpdate = "UPDATE dbo.cliente SET nome = '" + nome + "' WHERE id=" + id;
+                        SqlCommand comandoUpadate = new SqlCommand(stringUpdate, conexao);
+                        comandoUpadate.CommandType = CommandType.Text;
+                        comandoUpadate.ExecuteNonQuery();
+                        conexao.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (operacao == "deletar")
+                {
+                    try
+                    {
+                        Console.Write("id da conta que deseja deletar: ");
+                        int id = int.Parse(Console.ReadLine());
+                        conexao.Open();
+                        string stringDeletar = "DELETE FROM dbo.cliente WHERE id=" + id;
+                        SqlCommand comandoDeletar = new SqlCommand(stringDeletar, conexao);
+                        comandoDeletar.CommandType = CommandType.Text;
+                        comandoDeletar.ExecuteNonQuery();
+                        conexao.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (operacao == "exibir") {
+                    try
+                    {   conexao.Open();
+                        Console.WriteLine("exibir todos os dados:");
+                        string stringDeExibicao = "select * FROM dbo.cliente";
+                        SqlCommand comandoExibicao = new SqlCommand(stringDeExibicao, conexao);
+                        comandoExibicao.CommandType = CommandType.Text;
+                        SqlDataReader retornoExibicao = comandoExibicao.ExecuteReader();
+                        while (retornoExibicao.Read())
+                        {
+                            Cliente cliente = new Cliente(Int32.Parse(retornoExibicao[0].ToString()), retornoExibicao[1].ToString(), Int32.Parse(retornoExibicao[2].ToString()));
+                            Console.Write(cliente+"\n\n\n");
+                            
+                        }
+                        conexao.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine();
+                    }
+
+                }
+                else if (operacao=="sair") {
+                    sair = false;                
+                }            
+                
+            }
+            
+        }
+    }
+}
